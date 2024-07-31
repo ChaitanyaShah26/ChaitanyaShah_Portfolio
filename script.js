2abbf586b61193c3f063a80c5d6a6b61e368ca65
Chaitanya Shah <sendittochaitanya@gmail.com>
2024-07-31 21:17:07 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("BackToTop").style.display = "block";
+  } else {
+    document.getElementById("BackToTop").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+function showSidebar()
+{
+	const sidebar = document.querySelector('.sidebar');
+	sidebar.style.display = 'flex';
+}
+
+function hideSidebar()
+{
+	const sidebar = document.querySelector('.sidebar');
+	sidebar.style.display = 'none';
+}
+
+function resumeOpen()
+{
+	window.open('https://drive.google.com/file/d/1CryuaOKrQS4uiB6Dao05-RIw9FLgJgXU/view?usp=sharing','_blank');
+}
+
+function navtoContact() 
+{
+	window.location.href='contact.html';
+}
